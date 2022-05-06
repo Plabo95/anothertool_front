e3aac4b5d4696c9f801e7c520ccc2ce7b1421b14
unknown <plabonovo@gmail.com>
2022-05-06 21:06:33 +0200

    formik en clients

--- src/forms/ClientForm.jsx
+++ src/forms/ClientForm.jsx
@@ -1,55 +1,28 @@
 import React, {useState, useEffect} from 'react'
-import { Button, useToast, Flex, FormControl,FormErrorMessage,FormHelperText,Input} from '@chakra-ui/react'
+import { Button, useToast, Flex, FormControl,FormErrorMessage,VStack,Input, FormLabel} from '@chakra-ui/react'
 import {DrawerBody,DrawerFooter} from '@chakra-ui/react'
+import * as Yup from 'yup';
+import {Field, Formik, useFormik} from "formik";
+import TextField from './TextField';
 
 function ClientForm({onClose, clients, client, setClients}){
     
-    //Solo inicializo estado si estoy editando un servicio service?
-    const[name, setName] = useState()
-    const[car, setCar] = useState()
-    const[telf, setTelf] = useState()
-
     const toast = useToast()
     const[loadingDelete, setLoadingDelete] = useState(false)
     const[loadingCreate, setLoadingCreate] = useState(false)
-
-    useEffect(() => {
-        if(client){
-            setName(client.name)
-            setCar(client.car)
-            setTelf(client.telf)
-        }
-        else{
-            setName()
-            setCar()
-            setTelf() }
-    }, [client]);
-
-    const onChangeName = (e) => {
-        setName(e.target.value) 
-      };
-
-    const onChangeCar = (e) => {
-        setCar(e.target.value) 
-      };
-    const onChangeTelf = (e) => {
-        //console.log(e.hex)
-        setTelf(e.target.value)
-      };
-        
+      
     function closeDrawer(){
         setLoadingDelete(false)
         setLoadingCreate(false)
         onClose()
     }
     
-    const handleSubmit = async(e) => {
+    const handleSubmit = async(values) => {
     setLoadingCreate(true)
-    e.preventDefault()
         const clientToCreate ={
-                'name': name,
-                'car': car,
-                'telf': telf,
+                'name': values.name,
+                'car': values.car,
+                'telf': values.telf,
         }
     const response = await fetch('https://plabo.pythonanywhere.com/api/createclient',{
         method: 'POST',
@@ -67,6 +40,8 @@ function ClientForm({onClose, clients, client, setClients}){
         isClosable: true,
         }) 
         const newdata= await response.json()
+        if(client){ //Solo si esta editando
+        setClients(clients.filter(item => item.id!==client.id)) } //creo una lista con todos menos el editado
         setClients((clients) => [...clients, newdata])
         closeDrawer()
         }
@@ -81,68 +56,39 @@ function ClientForm({onClose, clients, client, setClients}){
                 closeDrawer()
             }
     }
-    const  handleUpdate = async(e) => {
-        e.preventDefault()
-        const clientToUpdate ={
-            'name': name,
-            'car': car,
-            'telf': telf,
-    }
-    const response = await fetch('https://plabo.pythonanywhere.com/api/updateclient/'+client.id+'/',{
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(clientToUpdate)
-        })
-        const rstatus = response.status
-        if(rstatus >= 200 && rstatus<300){
-            toast({
-            title: 'Cliente guardado',
-            status: 'success',
-            duration: 6000,
-            isClosable: true,
-            }) 
-        const newdata= await response.json()
-        setClients(clients.filter(item => item.id!==client.id)) //creo una lista con todos menos el editado
-        setClients((clients) => [...clients, newdata]) //añado el editado a la lista
-        closeDrawer()
-        }
-        else{
-            toast({
-                title: 'Error al guardar ',
-                description: "Código de error"+ rstatus +' intentalo mas tarde' ,
-                status: 'error',
-                duration: 6000,
-                isClosable: true,
-                })
-                closeDrawer()
-              }
-    }
-
     return(
+        <Formik
+        initialValues= {{name: client? client.name : '' ,car: client? client.car: '',telf: client? client.telf: ''}}
+        validationSchema = {Yup.object({
+            name: Yup.string().required("Nombre es obligatorio"),
+            car: Yup.string().required("Coche es obligatorio"),
+            telf: Yup.string().required("Coche es obligatorio")
+            .min(9, "Debe ser de 9 dígitos")
+            .max(9, "Debe se de 9 dígitos"),
+        })}
+        onSubmit= {(values, actions) => {
+            handleSubmit(values)
+            actions.resetForm()
+        }}
+        >
+        {formik => (
         <>
-        <DrawerBody> 
-            <FormControl>
-                <label> Nombre </label>
-                <Input onChange={onChangeName} placeholder={client? client.name : ' ' }/>
-                <label> Coche </label>
-                <Input type="text" onChange={onChangeCar} placeholder={client? client.car: 'Lexus IS200'}/>
-                <label> Teléfono </label>
-                <Input type="text" onChange={onChangeTelf} placeholder={client? client.telf: ''}/>
-            </FormControl>
-
+        <DrawerBody>        
+            <VStack>
+                <TextField label="Nombre" name="name" />
+                <TextField label="Coche" name="car" />
+                <TextField label="Teléfono" name="telf" />
+            </VStack>      
         </DrawerBody>
         <DrawerFooter>
           <Flex justify="right" columnGap="3" mt='3'>
-              <Button variant='ghost' colorScheme='red' size='sm' onClick={onClose}>Cancel</Button>
-              {client? 
-              <Button colorScheme='orange' size='sm' onClick={handleUpdate} isLoading={loadingCreate} loadingText='Guardando'>  Update </Button>: 
-              <Button colorScheme='orange' size='sm' onClick={handleSubmit} isLoading={loadingCreate} loadingText='Guardando'>  Create </Button>}
+              <Button variant='ghost' colorScheme='red' size='sm' onClick={onClose}>Cancelar</Button>
+              <Button colorScheme='orange' size='sm' onClick={formik.handleSubmit} isLoading={loadingCreate} loadingText='Guardando'>  Guardar </Button>
           </Flex>  
         </DrawerFooter>
         </>
+            )}
+        </Formik>
     )
 }
-
 export default ClientForm;
