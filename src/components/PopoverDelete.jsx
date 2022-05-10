22312ea2b99c1409ba0105c5e3879ea4f005e5e4
Plabo95 <plabonovo@example.com>
2022-05-10 12:53:11 +0200

    na

--- src/components/PopoverDelete.jsx
+++ src/components/PopoverDelete.jsx
@@ -1,15 +1,15 @@
-import React, {useState} from 'react'
+import React, {useState, useRef} from 'react'
 import {Popover,PopoverTrigger,PopoverContent, PopoverHeader, PopoverBody,PopoverFooter,PopoverArrow,PopoverCloseButton, useDisclosure, ButtonGroup} from '@chakra-ui/react'
 import {Button} from '@chakra-ui/react'
 
 export default function PopoverDelete({onDelete, id}){
 
-    const initialFocusRef = React.useRef()
+    const initialFocusRef = useRef()
     const {isOpen, onOpen, onClose } = useDisclosure()
 
     return(
         <>
-        <Popover onClose={onClose} initialFocusRef={initialFocusRef}>
+        <Popover onClose={onClose}  initialFocusRef={initialFocusRef}>
             <PopoverTrigger>
                 <Button colorScheme='red' size='xs' type="button">X</Button>
             </PopoverTrigger>
