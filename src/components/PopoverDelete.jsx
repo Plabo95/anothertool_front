24b4c681aecc4ee3c1b2f6d29e6ac30809c7c357
kalomcode <carloscalzadacabano@gmail.com>
2022-05-11 11:24:10 +0200

    kalom - boton cancelar y bordeado focus

--- src/components/PopoverDelete.jsx
+++ src/components/PopoverDelete.jsx
@@ -5,28 +5,32 @@ import {Button} from '@chakra-ui/react'
 export default function PopoverDelete({onDelete, id}){
 
     const initialFocusRef = React.useRef()
-    const {isOpen, onOpen, onClose } = useDisclosure()
+    const {isOpen, onOpen, onClose, onCancel } = useDisclosure()
 
     return(
         <>
         <Popover onClose={onClose} initialFocusRef={initialFocusRef}>
+        {({ onClose }) => (
+            <>
             <PopoverTrigger>
                 <Button colorScheme='red' size='xs' type="button">X</Button>
             </PopoverTrigger>
             <PopoverContent>
                 <PopoverArrow />
-                    <PopoverCloseButton />
-                    <PopoverBody>Seguro que quieres borrarlo?</PopoverBody>
-                    <PopoverFooter border='0' d='flex' alignItems='center' justifyContent='flex-end' pb={4}>
-                    <ButtonGroup>
-                    <Button variant='outline' size='sm' colorScheme='orange' onClick={onClose} > Cancelar </Button>
-                    <Button variant='ghost' size='sm' colorScheme='red' ref={initialFocusRef} onClick={() => onDelete(id)}>
-                    Sí
-                    </Button>
-                    </ButtonGroup>                        
-                    </PopoverFooter>
-                </PopoverContent>
-            </Popover>
+                <PopoverCloseButton />
+                <PopoverBody>Seguro que quieres borrarlo?</PopoverBody>
+                <PopoverFooter border='0' d='flex' alignItems='center' justifyContent='flex-end' pb={4}>
+                <ButtonGroup>
+                <Button variant='outline' size='sm' colorScheme='orange' onClick={onClose} > Cancelar </Button>
+                <Button variant='ghost' size='sm' colorScheme='red' ref={initialFocusRef} onClick={() => onDelete(id)}>
+                Sí
+                </Button>
+                </ButtonGroup>                        
+                </PopoverFooter>
+            </PopoverContent>
+            </>
+            )}
+        </Popover>
         </>
     )
 }
