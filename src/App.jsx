85a40e63863f0da37d2b6a491175c76873e68fd3
Plabo95 <plabonovo@example.com>
2022-05-30 15:10:42 +0200

    routing weno

--- src/App.jsx
+++ src/App.jsx
@@ -33,9 +33,9 @@ function App() {
           <Navbar/>   
           <Routes>
               <Route index element={<Landing/>} />
-              <Route path='klndr_front/' index element={<Landing/>} />  
+              <Route path='/' index element={<Landing/>} />  
               <Route path='klndr_front/register' element={<Register/>} /> 
-              <Route path='klndr_front/login' element={<Login/>} />  
+              <Route path='/login' element={<Login/>} />  
 
               <Route element={<AdminRoute/>}>
                 <Route path="klndr_front/adminpanel" element={<AdminPanel/>} />
