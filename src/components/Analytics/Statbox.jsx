f5d5dac50d8fd6a20302f0fdea41162585580002
kalomcode <carloscalzadacabano@gmail.com>
2022-06-07 10:49:36 +0200

    mismo ancho analiticas

--- src/components/Analytics/Statbox.jsx
+++ src/components/Analytics/Statbox.jsx
@@ -5,16 +5,16 @@ export default function Statbox({title, data}){
 
 
     return(
-    <Flex direction='column' minW='10em' gap='2em' p='2em' bg='white' justify='center' align='center' rounded='xl'boxShadow={'md'}>  
-    <Stat>
-        <Text>{title}</Text>
-        <Heading>{data}</Heading>
+        <Flex direction='column' w='200px' gap='2em' p='2em' bg='white' justify='center' align='center' rounded='xl'boxShadow={'md'}>  
+            <Stat>
+                <Text>{title}</Text>
+                <Heading>{data}</Heading>
 
-        <StatHelpText>
-            <StatArrow type='increase' />
-            5.46%
-        </StatHelpText>
-    </Stat>
-    </Flex>
+                <StatHelpText>
+                    <StatArrow type='increase' />
+                    5.46%
+                </StatHelpText>
+            </Stat>
+        </Flex>
     )
 }
