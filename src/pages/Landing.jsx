b9e79ee7a5480f9ddbc9adcd25dc27d30f719f6b
Plabo95 <pablo.novo@ara-tech.es>
2022-08-19 14:52:08 +0200

    landing_good

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,11 +1,16 @@
 import {Flex, Heading, Text, Button, Box, Image, IconButton} from '@chakra-ui/react'
 import AuthContext from '../auth/AuthContext'
 
+//images
+import gradient from '../assets/landing/gradient.jpg'
 import imgLogo from '../assets/logo_navbar.png'
-import img1 from '../assets/landing1_img1.png'
-import img2 from '../assets/landing2_img1.png'
-import img3 from '../assets/landing3_img1.png'
-import img5 from '../assets/landing5_img1.png'
+import img1 from '../assets/landing/img1.png'
+import img2 from '../assets/landing/img2.png'
+import img3 from '../assets/landing/img3.png'
+import img4 from '../assets/landing/img4.png'
+import img5 from '../assets/landing/img5.png'
+import img6 from '../assets/landing/img6.png'
+import tick from '../assets/landing/tick.png'
 
 import {AiOutlineCalendar, AiOutlineUnorderedList} from 'react-icons/ai';
 import {MdOutlinePeopleAlt} from 'react-icons/md';
@@ -19,7 +24,6 @@ export default function Landing(){
 
     const {user, authTokens, logoutUser} = useContext(AuthContext)
     const p_franja = '5%'
-    const h_franja = '85vh'
     const icon_box = '120px'
     const navigate = useNavigate();
 
@@ -41,7 +45,7 @@ export default function Landing(){
 
 
     return(
-        <Flex direction='column' w='100%' >
+        <Flex direction='column' w='100%' bgImage={gradient} align='center'>
 
             <Flex bg='white' height='8vh' w='100%' p='1%' align='center' position='fixed' zIndex={3}>
                 <Flex w='50%' justify='start' ml='3%'>
@@ -58,68 +62,111 @@ export default function Landing(){
                     >Pruébalo gratis</Button>
                 </Flex>
             </Flex>
-
-            <Flex bg='white' w='100%'  p={p_franja} /* height={h_franja} */> 
-                <Flex direction='column' w={['100%','100%','100%','100%','70%']} ml='5%' >
-                    <Flex justifyContent={['center','center','center','space-between','start']}>
-                        <Box>
-                            <Flex><Heading variant='h1'>another</Heading><Heading variant='h1' color={'orange'}>day</Heading></Flex>
-                            <Flex><Heading variant='h1'>another</Heading><Heading variant='h1' color={'blue'}>tool</Heading></Flex>
-                        </Box>
-                        <Box display={['none','none','none','block','none']}>
-                            <Image h='100%' w='xs' src={img1}/>
-                        </Box> 
-                    </Flex>
-                    <Heading my='3%' variant='h2' >Software de gestión para tu taller mecánico</Heading>
-                    <Text variant='parrafo' my='4%' >Administra tu taller online de manera muy intuitiva y sencilla</Text>
-                    <Flex>
-                        <Button variant='primary-s' size='lg' 
+            {/* Pantalla 1 */}
+            <Flex w='80%' justify='center' gap='5%' my='8vh' align='center' minH='82vh'>
+                <Flex direction='column' gap='1em' maxW='30%' >
+                    <Flex><Heading variant='h1'>another</Heading><Heading variant='h1' color={'orange'}>day</Heading></Flex>
+                    <Flex><Heading variant='h1'>another</Heading><Heading variant='h1' color={'blue'}>tool</Heading></Flex>
+                    <Text mt='1em' fontSize='22px' >Administra tu taller online de manera muy intuitiva y sencilla</Text>
+                    <Button mt='1em' variant='primary-s' w='45%' 
                         onClick={() => navigate('register')}
-                        >Pruébalo gratis</Button>
-                    </Flex>
+                        >Pruébalo gratis
+                    </Button>
                 </Flex>
-                <Flex alignItems='center' display={['none','none','none','none','flex']}>
-                    <Box p='5%'>
-                        <Image src={img1}/>
-                    </Box>   
-                </Flex>
+                <Flex>
+                    <Image  src={img1}/>
+                </Flex>    
+            </Flex>
+            {/* PANTALLA  2 */}
+            <Flex w='80%' minH='75vh'>
+                <Flex direction='column' align='center'>
+                    <Heading variant='h2' justifyContent='center' >Un software eficiente y organizado</Heading>
+                    <Image src={img2}/>        
+                    <Text maxW='40%' fontSize='22px' textAlign='center' mt='3%' >
+                        No pierdas más el tiempo con programas liosos, llenos de pestañas con funciones inservibles.
+                        anothertool quiere facilitarte el trabajo con un diseño intuitivo y actual.
+                    </Text>
+                    <Button mt='1em' variant='primary-s' 
+                        onClick={() => navigate('register')}
+                        >Conoce anothertool
+                    </Button> 
+                </Flex> 
             </Flex>
 
-            <Flex bg='lightgray' w='100%' /* height={h_franja} */ p={p_franja}  direction='column' align='center'>
-                <Heading variant='h2' justifyContent='center' >Un software eficiente y organizado</Heading>
-                <Image src={img2}/>        
-                <Text variant='parrafo' w={['100%','100%','90%','90%','75%',]} textAlign='center' mt='3%' >
-                    No pierdas más el tiempo con programas liosos, llenos de pestañas con funciones inservibles.
-                    anothertool quiere facilitarte el trabajo con un diseño intuitivo y actual.
-                </Text>
+            {/* PANTALLA 3 */}
+            <Flex w='80%' align='center' gap='5%' justify='center' minH='75vh'>
+                <Flex direction='column' maxW='40%' gap='2em'>
+                    <Heading variant='h2'>
+                        Personalizado a medida para tu negocio
+                    </Heading>
+                    <Text fontSize='22px'>
+                    Estamos muy comprometidos con el mundo del taller, estando al día de las necesidades 
+                    de nuestros clientes, actualizándonos día a día. 
+                    </Text>
+                    <Button mt='1em' variant='primary-s' w='40%'
+                        onClick={() => navigate('register')}
+                        >Pruébalo gratis
+                    </Button>
+                </Flex>
+                <Flex>
+                    <Image  src={img3}/>
+                </Flex>    
             </Flex>
 
-            <Flex bg='white' w='100%' p={p_franja} /* height={h_franja} */ align='center' >
-                <Flex w='50%'>
-                    <Image src={img3}/>  
-                </Flex>
-                <Flex w='50%' direction='column' gap='3'>
-                    <Flex>
-                        <Text variant='parrafo'>Prioriza la experiencia del usuario</Text>
+            {/* PANTALLA 4 */}
+            <Flex w='80%' align='center' gap='5%' justify='center' minH='75vh'>
+                <Flex>
+                    <Image  src={img4}/>
+                </Flex> 
+                <Flex direction='column' maxW='40%' gap='2em'>
+                    <Flex align='center' gap='1em'>
+                        <Image  src={tick}/>
+                        <Text fontSize='22px'>Prioriza la experiencia del usuario</Text>
                     </Flex>
-                    <Flex>
-                        <Text variant='parrafo'>Fácil e intuitivo</Text>
+                    <Flex align='center' gap='1em'>
+                        <Image  src={tick}/>
+                        <Text fontSize='22px'>Fácil e intuitivo</Text>
                     </Flex>
-                    <Flex>
-                        <Text variant='parrafo'>Todo en una plataforma online</Text>
+                    <Flex align='center' gap='1em'>
+                        <Image  src={tick}/>
+                        <Text fontSize='22px'>Todo en una plataforma online</Text>
                     </Flex>
-                    <Flex>
-                        <Text variant='parrafo'>Controla tu taller desde cualquier lugar</Text>
+                    <Flex align='center' gap='1em'>
+                        <Image  src={tick}/>
+                        <Text fontSize='22px'>Controla tu taller desde cualquier lugar</Text>
                     </Flex>
-                </Flex>
+                </Flex>   
             </Flex>
 
-            <Flex bg='lightgray' w='100%' direction='column' align='center' /* height={h_franja} */ p={p_franja}>
-                <Heading variant='h2' mx='5%'>anothertool, tu otra herramienta online</Heading>      
-                <Text variant='parrafo' mx='5%'>Calendario, seguimiento de trabajos, historial de cliente,s gestión de citas, tareas y servicios
-                    estadísticas... 
-                </Text>
-                <Flex direction={['column','column','column','row','row']} gap='10' mt='5%' >
+            {/* PANTALLA 5 */}
+            <Flex w='80%' align='center' gap='5%' justify='center' minH='75vh'>
+                <Flex direction='column' maxW='30%' gap='2em'>
+                    <Heading variant='h2'>
+                        Lleva un seguimiento de tu taller
+                    </Heading>
+                    <Text fontSize='22px'>
+                    Controlola tu inventario y estate al día de tus ingresos 
+                    y del funcionamiento de tu taller, para elevar tu negocio
+                    </Text>
+                    <Button mt='1em' variant='primary-s' w='40%'
+                        onClick={() => navigate('register')}
+                        >Pruébalo gratis
+                    </Button>
+                </Flex>
+                <Flex>
+                    <Image  src={img5}/>
+                </Flex>    
+            </Flex>
+
+            {/* PANTALLA 6 */}
+            <Flex w='80%' direction='column' gap='10%' align='center' justify='center' minH='90vh' >
+                <Flex maxW='60%' direction='column' align='center' gap='2em'>
+                    <Heading fontSize='40px'>anothertool, tu otra herramienta online</Heading>      
+                    <Text fontSize='22px' align='center'>Calendario, seguimiento de trabajos, historial de cliente,s gestión de citas, tareas y servicios
+                        estadísticas... 
+                    </Text>
+                </Flex>
+                <Flex direction={['column','column','column','row','row']} gap='10'>
                     <Flex gap='10'>
                         <Flex bg='white' w={icon_box} h={icon_box} rounded='xl' align='center' justify= 'center'>
                             <AiOutlineCalendar className='svg-blue' size='50%' />
@@ -145,20 +192,34 @@ export default function Landing(){
                 </Flex>
             </Flex>
 
-            <Flex bg='white' w='100%' /* height={h_franja} */ direction={['column','column','column','column','row']}  p={p_franja}>
-                <Flex w={['100%','100%','100%','100%','70%']} ml='5%' direction='column' gap='4' >
-                    <Heading variant='h1'>Personalizado a medida para tu negocio</Heading>      
-                    <Text variant='parrafo'>
-                        Estamos muy comprometidos con el mundo del taller,
-                        estando al día de las necesidades de nuestros clientes,
-                        actualiándonos día a día.
-                    </Text>
-                </Flex> 
-                <Flex alignItems='center' justifyContent='center'>
-                    <Box p='5%' w={['70%','60%','50%','40%','100%']}>
-                        <Image src={img5}/>
-                    </Box>   
+            {/* FOOTER */}
+            <Flex w='100%' direction='column' align='center' bg='#F0F0F0' py='5em'>
+                <Flex w='80%' justify='space-around'>
+                    <Flex direction='column' maxW='20%' gap='2em'>
+                        <Image boxSize='80%' src={imgLogo}/>
+                        <Text fontSize='20px' >Software de gestión para tu taller mecánico</Text>
+                    </Flex>
+                    <Flex direction='column' maxW='20%' gap='2em'>
+                        <Text fontSize='30px' >Empresa</Text>
+                        <Text fontSize='20px' >Servicios</Text>
+                        <Text fontSize='20px' >precios</Text>
+                    </Flex>
+                    <Flex direction='column' maxW='20%' gap='2em'>
+                        <Text fontSize='30px' >Contacto</Text>
+                        <Text fontSize='20px' >anothertool.app@gmail.com</Text>
+                        <Text fontSize='20px' >precios</Text>
+                    </Flex>
                 </Flex>
+                <Flex w='80%'  justify='space-around' mt='5em'>
+                    <Text fontSize='20px' >anothertool</Text>
+                    <Text fontSize='20px' >|</Text>
+                    <Text fontSize='20px' >Términos de uso</Text>
+                    <Text fontSize='20px' >|</Text>
+                    <Text fontSize='20px' >Política de privacidad</Text>
+                    <Text fontSize='20px' >|</Text>
+                    <Text fontSize='20px' >Seguridad</Text>
+                </Flex>
+
             </Flex>
 
         </Flex>
