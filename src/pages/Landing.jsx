ba1a4f4aaf35f3339ef9b9fa6c9249ac4ca4b31a
Plabo95 <pablo.novo@ara-tech.es>
2022-08-19 21:18:53 +0200

    footer y autotype

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,6 @@
-import {Flex, Heading, Text, Button, Box, Image, IconButton} from '@chakra-ui/react'
+import {Flex, Heading, Text, Button, Box, Image} from '@chakra-ui/react'
 import AuthContext from '../auth/AuthContext'
+import ReactTypingEffect from 'react-typing-effect';
 
 //images
 import gradient from '../assets/landing/gradient.jpg'
@@ -23,7 +24,6 @@ import { useContext, useEffect } from 'react'
 export default function Landing(){
 
     const {user, authTokens, logoutUser} = useContext(AuthContext)
-    const p_franja = '5%'
     const icon_box = '120px'
     const navigate = useNavigate();
 
@@ -64,9 +64,21 @@ export default function Landing(){
             </Flex>
             {/* Pantalla 1 */}
             <Flex w='80%' justify='center' gap='5%' my='8vh' align='center' minH='82vh'>
-                <Flex direction='column' gap='1em' maxW='30%' >
-                    <Flex><Heading variant='h1'>another</Heading><Heading variant='h1' color={'orange'}>day</Heading></Flex>
+                <Flex direction='column' gap='1em' maxW='35%' >
+                    <Flex>
+                        <Heading variant='h1'>another</Heading>
+                        <Heading variant='h1' color={'orange'}>                    
+                        <ReactTypingEffect
+                            text={["day", "project", "car", "person"]}
+                            speed={100}
+                            eraseDelay={1500}
+                            typingDelay={1500}
+                        />
+                        </Heading>
+                    </Flex>
+                    
                     <Flex><Heading variant='h1'>another</Heading><Heading variant='h1' color={'blue'}>tool</Heading></Flex>
+
                     <Text mt='1em' fontSize='22px' >Administra tu taller online de manera muy intuitiva y sencilla</Text>
                     <Button mt='1em' variant='primary-s' w='45%' 
                         onClick={() => navigate('register')}
@@ -193,31 +205,31 @@ export default function Landing(){
             </Flex>
 
             {/* FOOTER */}
-            <Flex w='100%' direction='column' align='center' bg='#F0F0F0' py='5em'>
-                <Flex w='80%' justify='space-around'>
-                    <Flex direction='column' maxW='20%' gap='2em'>
-                        <Image boxSize='80%' src={imgLogo}/>
-                        <Text fontSize='20px' >Software de gestión para tu taller mecánico</Text>
+            <Flex w='100%' direction='column' align='center' bg='#F0F0F0' py='2em'>
+                <Flex w='80%' justify='space-around' mt='5em'>
+                    <Flex direction='column' maxW='20%' gap='1em'>
+                        <Image src={imgLogo}/>
+                        <Text fontSize='16px' >Software de gestión para tu taller mecánico</Text>
                     </Flex>
-                    <Flex direction='column' maxW='20%' gap='2em'>
-                        <Text fontSize='30px' >Empresa</Text>
-                        <Text fontSize='20px' >Servicios</Text>
-                        <Text fontSize='20px' >precios</Text>
+                    <Flex direction='column' maxW='20%' gap='1em'>
+                        <Text fontSize='20px' >Empresa</Text>
+                        <Text fontSize='16px' >Servicios</Text>
+                        <Text fontSize='16px' >precios</Text>
                     </Flex>
-                    <Flex direction='column' maxW='20%' gap='2em'>
-                        <Text fontSize='30px' >Contacto</Text>
-                        <Text fontSize='20px' >anothertool.app@gmail.com</Text>
-                        <Text fontSize='20px' >precios</Text>
+                    <Flex direction='column' maxW='20%' gap='1em'>
+                        <Text fontSize='20px' >Contacto</Text>
+                        <Text fontSize='16px' >anothertool.app@gmail.com</Text>
+                        <Text fontSize='16px' >precios</Text>
                     </Flex>
                 </Flex>
-                <Flex w='80%'  justify='space-around' mt='5em'>
-                    <Text fontSize='20px' >anothertool</Text>
-                    <Text fontSize='20px' >|</Text>
-                    <Text fontSize='20px' >Términos de uso</Text>
-                    <Text fontSize='20px' >|</Text>
-                    <Text fontSize='20px' >Política de privacidad</Text>
-                    <Text fontSize='20px' >|</Text>
-                    <Text fontSize='20px' >Seguridad</Text>
+                <Flex w='80%'  justify='center' gap='4em' mt='3em' py='1em'>
+                    <Text fontSize='16px' >anothertool</Text>
+                    <Text fontSize='16px' >|</Text>
+                    <Text fontSize='16px' >Términos de uso</Text>
+                    <Text fontSize='16px' >|</Text>
+                    <Text fontSize='16px' >Política de privacidad</Text>
+                    <Text fontSize='16px' >|</Text>
+                    <Text fontSize='16px' >Seguridad</Text>
                 </Flex>
 
             </Flex>
