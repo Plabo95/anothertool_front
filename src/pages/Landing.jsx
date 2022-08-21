e72774f1ee69480ab06e5259934dda3b7d1d3752
Plabo95 <pablo.novo@ara-tech.es>
2022-08-21 20:15:09 +0200

    a

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -24,6 +24,7 @@ import {RiBookletLine} from 'react-icons/ri';
 
 //components
 import NavbarLanding from '../components/Navbar/NavLanding';
+import Footer from '../components/Footer';
 
 export default function Landing(){
 
@@ -204,34 +205,7 @@ export default function Landing(){
             </Flex>
 
             {/* FOOTER */}
-            <Flex w='100%' direction='column' align='center' bg='#F0F0F0' py='2em'>
-                <Flex w='80%' justify='space-around' mt='5em'>
-                    <Flex direction='column' maxW='20%' gap='1em'>
-                        <Image src={imgLogo}/>
-                        <Text fontSize='16px' >Software de gestión para tu taller mecánico</Text>
-                    </Flex>
-                    <Flex direction='column' maxW='20%' gap='1em'>
-                        <Text fontSize='20px' >Empresa</Text>
-                        <Text fontSize='16px' >Servicios</Text>
-                        <Text fontSize='16px' >precios</Text>
-                    </Flex>
-                    <Flex direction='column' maxW='20%' gap='1em'>
-                        <Text fontSize='20px' >Contacto</Text>
-                        <Text fontSize='16px' >anothertool.app@gmail.com</Text>
-                        <Text fontSize='16px' >precios</Text>
-                    </Flex>
-                </Flex>
-                <Flex w='80%'  justify='center' gap='4em' mt='3em' py='1em'>
-                    <Text fontSize='16px' >anothertool</Text>
-                    <Text fontSize='16px' >|</Text>
-                    <Text fontSize='16px' >Términos de uso</Text>
-                    <Text fontSize='16px' >|</Text>
-                    <Text fontSize='16px' >Política de privacidad</Text>
-                    <Text fontSize='16px' >|</Text>
-                    <Text fontSize='16px' >Seguridad</Text>
-                </Flex>
-
-            </Flex>
+            <Footer/>
 
         </Flex>
     )
