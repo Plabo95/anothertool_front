35a22e7ca8534759301fb2ede54ee75c07e86e94
pablo.novo <pablo.novo@ara-tech.es>
2023-01-11 18:08:43 +0100

    login api

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,54 @@
-import { Button, Flex, Heading, Text} from '@chakra-ui/react'
-import * as Yup from 'yup';
-import {Formik} from "formik";
-import TextField from '../components/forms/TextField'
+import { Button, Flex, Heading, Text, useToast} from '@chakra-ui/react'
 import {useNavigate} from 'react-router-dom'
 
 //images
 import bg from '../img/login-register/login_bg.jpg'
 //Components
 import NavLoginRegister from '../components/loginRegister/NavLoginRegister';
+//validation
+import * as Yup from 'yup';
+import {Formik} from "formik";
+import TextField from '../components/forms/TextField'
+//api
+import { login } from '../api/authApi';
+import { useMutation } from "@tanstack/react-query"
+//auth
+import { useSignIn } from "react-auth-kit"
+
 
 export default function Login(){
 
     const navigate = useNavigate()
+    const toast = useToast()
+    const signIn = useSignIn()
+
+    const {isLoading, mutate} = useMutation(
+        ["login"],
+        login,
+        {
+        onSuccess: (data) => {
+            toast({title: 'Login exitoso!',status:"success"})
+            signIn({
+                token: data.access_token,
+                expiresIn: 3600,
+                tokenType: "Bearer",
+                authState: {username: 'gervasio'},           
+            })
+            navigate('/')
+        },
+        onError : (error)=>{
+            toast({title: error.message, description: error.response.data?.message ,status:"error"})
+        }
+        }
+    );
+
+    //vaaalidation for login
+    const LoginSchema = Yup.object({
+        password: Yup.string()
+            .min(6, 'Demasiado corta')
+            .required('Es obligatorio'),
+        username: Yup.string().email('Formato de email inválido').required('Es obligatorio'),
+        });  
 
     return(
         <Flex w='100%' height='100vh' direction='column' 
@@ -22,60 +59,52 @@ export default function Login(){
             bgSize='100%'>
 
             <NavLoginRegister/>
-
             <Flex justify='center' align='center' w='100%'>
-
                    <Flex py='5%'  w={['80%','75%','400px','400px']} direction='column' align='center' gap='5'>
-                        <Flex><Heading size='lg' >another</Heading><Heading size='lg' color={'blue'}>tool</Heading></Flex>
-                            <Flex bg='white' w='100%' rounded='xl' direction='column' align='center'  gap='6' py='12%' boxShadow='lg'>
-                                <Heading size='md'> ¡Hola de nuevo! </Heading>
-                                <Formik
-                                initialValues = {{
-                                    username: "",
-                                    password: "",
-                                    record: false
-                                    }
+                    <Flex><Heading size='lg' >another</Heading><Heading size='lg' color={'blue'}>tool</Heading></Flex>
+                        <Flex bg='white' w='100%' rounded='xl' direction='column' align='center'  gap='6' py='12%' boxShadow='lg'>
+                            <Heading size='md'> ¡Hola de nuevo! </Heading>
+                            <Formik
+                            initialValues = {{
+                                username: "",
+                                password: "",
                                 }
-                                validationSchema = {Yup.object({
-                                    username: Yup.string().required("Nombre es obligatorio"),
-                                    password: Yup.string().required("Coche es obligatorio"),
-                                    
-                                })}
-                                onSubmit= {async (values, actions) => {
-                                    //alert(JSON.stringify(values))
-                                    actions.resetForm()                 
+                            }
+                            validationSchema={LoginSchema}
+                            onSubmit={(values) => {
+                                console.log(values)
+                                mutate(values);
                                 }}
-                                >
-                                {formik => (
-                                <Flex direction={'column'} onKeyDown={(e)=> {if(e.key === "Enter"){formik.handleSubmit()}}} as="form" w='80%' justify='space-around' align='center' gap='3'>
-                                <TextField name="username" placeholder="Usuario"  />
-                                <TextField type="password" name="password" placeholder="Contraseña" />
-                                <Button mt='8' variant='primary-s' size='md'
-                                onClick={formik.handleSubmit}  loadingText='Iniciando...'>
-                                    Iniciar Sesión </Button> 
-                                </Flex>
-                                    )}
-                                </Formik>
+                            >
+                            {formik => (
+                            <Flex direction={'column'} onKeyDown={(e)=> {if(e.key === "Enter"){formik.handleSubmit()}}} as="form" w='80%' justify='space-around' align='center' gap='3'>
+                            <TextField name="username" placeholder="Usuario"  />
+                            <TextField type="password" name="password" placeholder="Contraseña" />
+                            <Button mt='8' variant='primary-s' size='md'
+                            onClick={formik.handleSubmit}  isLoading={isLoading}  >
+                                Iniciar Sesión </Button> 
                             </Flex>
-                        
-                            {/* 
-                            <Flex bg='white' w='100%' rounded='xl' direction='column' align='center'  gap='6' py='12%' boxShadow='lg'>
-                                <Heading size='md'> Ya existe una sesión iniciada </Heading>
-                                <Button mt='8' variant='primary-out-s' size='md'>
-                                        Cerrar Sesión </Button> 
-                            </Flex>
-                            */}
-                        
-                        <Flex w='100%' justify='space-between' >
-                            <Text fontSize='xs' cursor="pointer"  onClick={() => navigate('/register')}>
-                                Registrarse
-                            </Text>
-                            <Text fontSize='xs' cursor="pointer">
-                                He olvidado mi contraseña
-                            </Text>
+                                )}
+                            </Formik>
                         </Flex>
+                    
+                        {/* 
+                        <Flex bg='white' w='100%' rounded='xl' direction='column' align='center'  gap='6' py='12%' boxShadow='lg'>
+                            <Heading size='md'> Ya existe una sesión iniciada </Heading>
+                            <Button mt='8' variant='primary-out-s' size='md'>
+                                    Cerrar Sesión </Button> 
+                        </Flex>
+                        */}
+                    
+                    <Flex w='100%' justify='space-between' >
+                        <Text fontSize='xs' cursor="pointer"  onClick={() => navigate('/register')}>
+                            Registrarse
+                        </Text>
+                        <Text fontSize='xs' cursor="pointer">
+                            He olvidado mi contraseña
+                        </Text>
                     </Flex>
-            
+                </Flex>
             </Flex>
         </Flex>
     )
