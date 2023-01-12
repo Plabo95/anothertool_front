a5a4dc9e49bfe89bbae18fe9a500d5571a179a10
pablo.novo <pablo.novo@ara-tech.es>
2023-01-12 11:22:36 +0100

    urls to local

--- src/api/authApi.jsx
+++ src/api/authApi.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const authApi = axios.create({
-    baseURL: process.env.REACT_APP_API_URL
+    baseURL: process.env.REACT_APP_API_URL +'/user'
 })
 
 export const login = async(credentials) => {
