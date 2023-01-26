069401690fd82a6beba2314a3b0ef740e3d0544c
plabo <plabonovo@gmail.com>
2023-01-26 19:23:19 +0100

    carstable no errors

--- src/components/myGarage/tables/CarsTable.jsx
+++ src/components/myGarage/tables/CarsTable.jsx
@@ -1,75 +1,16 @@
-import React, {useState, useContext, useEffect,} from 'react'
 import {Table,Thead,Tbody,Tr,Th,Td,TableContainer,Button,useToast,IconButton, Flex,} from '@chakra-ui/react'
 import {Drawer,DrawerHeader,DrawerOverlay,DrawerContent,DrawerCloseButton,useDisclosure,} from '@chakra-ui/react'
 import SvgEdit from  './../dist/Edit'
 
-//Components
-import PopoverDelete from '../components/PopoverDelete'
-import CarForm from '../forms/CarForm'
-
-//api
-import carsApi from '../api/carsApi'
-import useApi from '../hooks/useApi'
-import AuthContext from '../auth/AuthContext'
-
 export default function CarsTable(){
-
-    const {user, authTokens} = useContext(AuthContext)
-    const getAllCarsApi = useApi(carsApi.getAllCars);
-    const deleteCarApi = useApi(carsApi.deleteCar);
 
     const toast = useToast()
     const { isOpen, onOpen, onClose } = useDisclosure()
-    const[cars, setCars] = useState([])
-    const[car, setCar] = useState()
-    const[creating, setCreating] = useState(false)
-
-    const updateTable = async () => {
-        const {data, error} = await getAllCarsApi.request(user,authTokens);
-        error? console.log('Error fetching...', error) 
-            : setCars(data)
-    }
-
-    //Clients
-    const handleDelete = async (e) =>{
-        const {error} = await deleteCarApi.request(e, user, authTokens)
-        if(!error){
-            toast({
-                title: 'Coche borrado',
-                status: 'success',
-                duration: 6000,
-                isClosable: true,
-            })
-            updateTable()
-        }   
-        else{
-            toast({
-                title: 'Error al borrar ',
-                description: "Código de error"+ error +' intentalo mas tarde' ,
-                status: 'error',
-                duration: 6000,
-                isClosable: true,
-            })
-        }    
-    }
-    function handleEdit(e){
-        setCar(e)
-        setCreating(false)
-        onOpen()
-    }
-    function handleCreate(){
-        setCar()
-        setCreating(true)
-        onOpen()
-    }
-    useEffect(() => {   
-        updateTable()
-    },[]) 
 
     return(
         <>
         <Flex justify='end'>
-            <Button variant='primary' onClick={()=>handleCreate()}>+ Añadir coche</Button>
+            <Button variant='primary'>+ Añadir coche</Button>
         </Flex>
         <Flex w="100%">
         <TableContainer mt='5' borderRadius='lg' w="100%" bg='white' boxShadow='lg'>
@@ -84,7 +25,7 @@ export default function CarsTable(){
                 </Tr>
             </Thead>
             <Tbody>
-                {cars.map(car=> {
+                {/* cars.map(car=> {
                 return(
                     <Tr key={car.idplate}>
                         <Td>{car.idplate}</Td>
@@ -96,7 +37,8 @@ export default function CarsTable(){
                             <PopoverDelete onDelete={handleDelete} id={car.idplate} />
                         </Td>    
                     </Tr>
-                )})}
+                )})
+                */}
             </Tbody>
         </Table>
         </TableContainer>
@@ -109,8 +51,10 @@ export default function CarsTable(){
             <DrawerOverlay />
             <DrawerContent>
             <DrawerCloseButton />
-            <DrawerHeader>{car? 'Editar Coche': 'Crear Coche' }</DrawerHeader> 
-                <CarForm  is_creating={creating} onClose={onClose} car={car} cars={cars} setCars={setCars} updateTable={updateTable}/>               
+            <DrawerHeader>Coche</DrawerHeader>
+                {/* 
+                <CarForm  onClose={onClose} /> 
+                */}              
             </DrawerContent>
         </Drawer>                    
      
