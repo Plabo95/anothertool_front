51986f2d8e198d2f1386978b3cef4b5beb367ba4
plabo <plabonovo@gmail.com>
2023-01-31 18:27:33 +0100

    services Api

--- src/api/servicesApi.jsx
+++ src/api/servicesApi.jsx
@@ -4,31 +4,31 @@ const servicesApi = axios.create({
     baseURL: process.env.REACT_APP_API_URL +'services'
 })
 
-export const getAllClients = async(token) => {
-    const response =  await clientsApi.get('/', 
+export const getAllServices = async(token) => {
+    const response =  await servicesApi.get('/', 
     {headers: {'Authorization': token}}
     );
     return response.data
 }
-export const createUpdateClient = async(payload) => {
+export const createUpdateService = async(payload) => {
     var response = {}
 
     if (payload.slug){
-        response =  await clientsApi.put('/'+payload.slug+'/' ,
+        response =  await servicesApi.put('/'+payload.slug+'/' ,
         payload.data,
         {headers: {'Authorization': payload.token}}
         );
     }
     else{
-        response =  await clientsApi.post('/',payload.data,
+        response =  await servicesApi.post('/',payload.data,
         {headers: {'Authorization': payload.token}}
         );
     }
 
     return response.data
 }
-export const deleteClient = async(payload) => {
-    const response =  await clientsApi.delete('/'+payload.slug+'/',
+export const deleteService = async(payload) => {
+    const response =  await servicesApi.delete('/'+payload.slug+'/',
     {headers: {'Authorization': payload.token}}
     );
     return response.data
